175d85fe22213295fd6de49c7cc744bed37eca7c
Sagar Raturi <sagarraturi85516@gmail.com>
2025-05-16 18:47:03 +0530

    Add forms and User Input

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '1e551a08ad2e9942c18480bdc42a8a00'
 posts = [
     {
         'author': 'Corey Schaefer',
@@ -24,8 +27,27 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def hello():
+def home():
     return render_template('home.html', posts =posts)
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='login', form=form)
 
 @app.route('/about')
 def about():
